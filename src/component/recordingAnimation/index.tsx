a6c0d85229d10aa46492b3e1382d36ad3f805084
s.shahrabi <saeed50002000@gmail.com>
2022-06-13 01:39:53 -0400

    RecordingAnimation component is written

---
+++ src/component/recordingAnimation/index.tsx
@@ -0,0 +1,11 @@
+const RecordingAnimation = () => {
+  return (
+    <div className="bounce">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default RecordingAnimation
